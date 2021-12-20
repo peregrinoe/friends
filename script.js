d1e36d403e6e05c32e5e62da1f9ff210f6c718d4
peregrinoe <s_nick_94@hotmail.com>
2021-12-20 00:35:45 -0600

    inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+//Llama el botón de menú
+let menu = document.querySelector('#list-navbar');
+let menu_bar = document.querySelector('#menu-navbar');
+
+    menu_bar.addEventListener('click', function() {
+        menu.classList.toggle('list-navbar-toggle');
+    })
