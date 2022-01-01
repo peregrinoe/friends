55d2593427a290b7873265079a9cb0c6795d95d9
peregrinoe <s_nick_94@hotmail.com>
2022-01-01 03:22:34 -0600

    Casi el front

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//Llama el botón de menú
+/*Llama el botón de menú
 let menu = document.querySelector('#list-navbar');
 let menu_bar = document.querySelector('#menu-navbar');
 
     menu_bar.addEventListener('click', function() {
         menu.classList.toggle('list-navbar-toggle');
     })
+*/
