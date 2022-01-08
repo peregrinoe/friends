6eed374cfb4e8c16ca3e658c9c3ded7d681c260f
peregrinoe <s_nick_94@hotmail.com>
2022-01-08 03:18:29 -0600

    Front Listo, Js en proceso

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-choose_question(0)
+let db_question = readText("questions.json")
+let interprete_db = JSON.parse(db_question)
+chooseQuestionRandom()
+
+//Escoger pregunta aleatoria 
+function chooseQuestionRandom() {
+    chooseQuestion(Math.floor(Math.random()*interprete_db.length))
+}
 //Leer preguntar y seleccionar una 
-function choose_question(n) {
-    let db_question = readText("questions.json")
-    let interprete_db = JSON.parse(db_question)
+function chooseQuestion(n) {
     pregunta = interprete_db[n]
     select_id("temporada").innerHTML = pregunta.temporada
     select_id("question").innerHTML = pregunta.pregunta
+    select_id("btn1").innerHTML = pregunta.respuesta
+    select_id("btn2").innerHTML = pregunta.incorrecta1
+    select_id("btn3").innerHTML = pregunta.incorrecta2
+    select_id("btn4").innerHTML = pregunta.incorrecta3
     select_id("image").setAttribute("src",pregunta.image)
+}
+//Desordenar las preguntas 
+function randomizeQuestion() {
+    
 }
 //Selección de objeto según Id
 function select_id(id) {
